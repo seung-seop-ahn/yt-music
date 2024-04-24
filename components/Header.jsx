cf66a904ce8bc3bd3447d0939f9e8ea2ec8b132b
seung-seop-ahn <seungseopahn@gmail.com>
2024-04-24 23:55:38 +0900

    feat: search

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Image from 'next/image'
+import UserIcon from '@/components/UserIcon'
+import PagePadding from '@/components/PagePadding'
+import { FaChromecast } from 'react-icons/fa'
+import { FiSearch } from 'react-icons/fi'
 
 const Header = ({ children }) => {
   return (
@@ -18,7 +22,31 @@ const Header = ({ children }) => {
           <div className={'absolute h-[400px] top-0 bg-gradient-to-t from-black w-full '}></div>
         </div>
       </section>
-      <section className={'absolute'}>{children}</section>
+      <secion className={'sticky'}>
+        <PagePadding>
+          <div className={'flex flex-row justify-between items-center h-[64px]'}>
+            <article
+              className={
+                'flex flex-row items-center h-[42px] min-w-[480px] bg-[rgba(0,0,0,0.14)] rounded-2xl px-[16px] gap-[16px]'
+              }
+            >
+              <div>
+                <FiSearch size={24} />
+              </div>
+              <input
+                className={'h-full w-full bg-transparent'}
+                type={'text'}
+                placeholder={'Music, Album, Artist, Podcast Search'}
+              />
+            </article>
+            <article className={'flex flex-row gap-6 items-center'}>
+              <FaChromecast size={26} />
+              <UserIcon />
+            </article>
+          </div>
+        </PagePadding>
+      </secion>
+      <section className={'relative'}>{children}</section>
     </header>
   )
 }
