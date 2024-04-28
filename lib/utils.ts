e732ae8bfddc405cb3c31803245108370b724c42
seung-seop-ahn <seungseopahn@gmail.com>
2024-04-28 19:19:59 +0900

    fix: random

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,7 +9,7 @@ export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms))
 
 export function getRandomInt(min: number, max: number) {
   min = Math.ceil(min);
-  max = Math.floor(min);
+  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
